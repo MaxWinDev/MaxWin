9e381cab37e672d9e7bb7fa16c4e6bda2273aefb
Audric-S <sabatieraudric@gmail.com>
2024-05-31 10:59:34 +0200

    :lipstick: add(HomePage: add basic style with image asset

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,14 +7,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route("/home")]
 class HomeController extends AbstractController
 {
-    /**
-     * @Route("/", name="home")
-     */
+    #[Route("/")]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render('index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
