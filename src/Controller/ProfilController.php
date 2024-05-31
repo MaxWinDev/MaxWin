79d1ac6431c9d7d5f4f0e884f55fc55555406422
Destroyeur6315 <romain.fillot630@gmail.com>
2024-05-31 16:07:35 +0200

    link database

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -5,14 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Service\ProfilManager;
 
 class ProfilController extends AbstractController
 {
     #[Route('/profil', name: 'app_profil')]
-    public function index(): Response
+    public function index(ProfilManager $profilManager): Response
     {
+        $user = $profilManager->getUserByID(1);
+        // console.log($user);
+        //var_dump($user);
+
         return $this->render('profil/index.html.twig', [
-            'controller_name' => 'ProfilController',
+            'user' => $user,
         ]);
     }
 }
