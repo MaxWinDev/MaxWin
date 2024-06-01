304ae5c8cbb5dd6774bea984d49877650c09fd02
Destroyeur6315 <romain.fillot630@gmail.com>
2024-06-01 14:19:10 +0200

    add shieldon and phpunit

---
+++ src/Controller/FirewallPanelController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class FirewallPanelController extends AbstractController
+{
+    #[Route('/firewall/panel', name: 'app_firewall_panel')]
+    public function panel()
+    {
+        $panel = new \Shieldon\Firewall\Panel();
+
+        // If your have `symfony/security-csrf` installed.
+        $csrf = $this->container->get('security.csrf.token_manager');
+        $token = $csrf->refreshToken('key')->getValue();
+
+        $panel->csrf(['_token' => $token]);
+        $panel->entry();
+        exit;
+    }
+
+    // #[Route('/firewall/panel/{class}/{method}', name: 'app_firewall_panel')]
+    // public function page(string $class, string $method): Response
+    // {
+    //     $this->panel($class, $method);
+
+    //     return new Response();
+    // }
+}
