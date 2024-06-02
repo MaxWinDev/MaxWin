2e8f529cf947194fe03d479f10c4497d2ab9b15b
thdupin2 <theo.dupin03300@gmail.com>
2024-06-02 12:16:32 +0200

    :bug: fix: Animation when spinning fruits

---
+++ public/js/index.js
@@ -0,0 +1,210 @@
+const cache = {};
+
+class Symbol {
+    constructor(name = Symbol.random()) {
+        this.name = name;
+
+        if (cache[name]) {
+            this.img = cache[name].cloneNode();
+        } else {
+            this.img = new Image();
+            this.img.src = `../assets/symbols/${name}.png`;
+
+            cache[name] = this.img;
+        }
+    }
+
+    static preload() {
+        Symbol.symbols.forEach((symbol) => new Symbol(symbol));
+    }
+
+    static get symbols() {
+        return [
+            "7",
+            "cerise",
+            "citron",
+            "fraise",
+            "gold",
+            "max",
+            "pasteque",
+            "prune",
+        ];
+    }
+
+    static random() {
+        return this.symbols[Math.floor(Math.random() * this.symbols.length)];
+    }
+}
+
+class Reel {
+    constructor(reelContainer, idx, initialSymbols) {
+        this.reelContainer = reelContainer;
+        this.idx = idx;
+
+        this.symbolContainer = document.createElement("div");
+        this.symbolContainer.classList.add("icons");
+        this.reelContainer.appendChild(this.symbolContainer);
+
+        this.animation = this.symbolContainer.animate(
+            [
+                // We cannot animate translateY & filter at the same time in safari for some reasons,
+                // so we go with animating top & filter instead.
+                { top: 0, filter: "blur(0)" },
+                { filter: "blur(2px)", offset: 0.5 },
+                {
+                    top: `calc((${Math.floor(this.factor) * 10} / 3) * -100% - (${
+                        Math.floor(this.factor) * 10
+                    } * 3px))`,
+
+                    filter: "blur(0)",
+                },
+            ],
+            {
+                duration: this.factor * 1000,
+                easing: "ease-in-out",
+            }
+        );
+        this.animation.cancel();
+
+        initialSymbols.forEach((symbol) =>
+            this.symbolContainer.appendChild(new Symbol(symbol).img)
+        );
+    }
+
+    get factor() {
+        return 1 + Math.pow(this.idx / 2, 2);
+    }
+
+    renderSymbols(nextSymbols) {
+        const fragment = document.createDocumentFragment();
+
+        for (let i = 3; i < 3 + Math.floor(this.factor) * 10; i++) {
+            const icon = new Symbol(
+                i >= 10 * Math.floor(this.factor) - 2
+                    ? nextSymbols[i - Math.floor(this.factor) * 10]
+                    : undefined
+            );
+            fragment.appendChild(icon.img);
+        }
+
+        this.symbolContainer.appendChild(fragment);
+    }
+
+    spin() {
+        const animationPromise = new Promise(
+            (resolve) => (this.animation.onfinish = resolve)
+        );
+        const timeoutPromise = new Promise((resolve) =>
+            setTimeout(resolve, this.factor * 1000)
+        );
+
+        this.animation.cancel();
+        this.animation.play();
+
+        return Promise.race([animationPromise, timeoutPromise]).then(() => {
+            if (this.animation.playState !== "finished") this.animation.finish();
+
+            const max = this.symbolContainer.children.length - 3;
+
+            for (let i = 0; i < max; i++) {
+                this.symbolContainer.firstChild.remove();
+            }
+        });
+    }
+}
+
+class Slot {
+    constructor(domElement, config = {}) {
+        Symbol.preload();
+
+        this.currentSymbols = [
+            ["max", "max", "max"],
+            ["max", "max", "max"],
+            ["max", "max", "max"],
+            ["max", "max", "max"],
+            ["max", "max", "max"],
+        ];
+
+        this.nextSymbols = [
+            ["max", "max", "max"],
+            ["max", "max", "max"],
+            ["max", "max", "max"],
+            ["max", "max", "max"],
+            ["max", "max", "max"],
+        ];
+
+        if (!domElement || !domElement instanceof HTMLElement) {
+            console.error("L'élément DOM passé en argument est invalide.");
+            return;
+        }
+
+        this.container = domElement;
+
+        this.reels = Array.from(this.container.getElementsByClassName("reel")).map(
+            (reelContainer, idx) =>
+                new Reel(reelContainer, idx, this.currentSymbols[idx])
+        );
+
+        this.spinButton = this.container.querySelector("#spin");
+        this.spinButton.addEventListener("click", () => this.spin());
+
+        this.autoPlayCheckbox = this.container.querySelector("#autoplay");
+
+        if (config.inverted) {
+            this.container.classList.add("inverted");
+        }
+
+        this.config = config;
+    }
+
+    spin() {
+        this.currentSymbols = this.nextSymbols;
+        this.nextSymbols = [
+            [Symbol.random(), Symbol.random(), Symbol.random()],
+            [Symbol.random(), Symbol.random(), Symbol.random()],
+            [Symbol.random(), Symbol.random(), Symbol.random()],
+            [Symbol.random(), Symbol.random(), Symbol.random()],
+            [Symbol.random(), Symbol.random(), Symbol.random()],
+        ];
+
+        this.onSpinStart(this.nextSymbols);
+
+        return Promise.all(
+            this.reels.map((reel) => {
+                reel.renderSymbols(this.nextSymbols[reel.idx]);
+                return reel.spin();
+            })
+        ).then(() => this.onSpinEnd(this.nextSymbols));
+    }
+
+    onSpinStart(symbols) {
+        this.spinButton.disabled = true;
+
+        this.config.onSpinStart?.(symbols);
+    }
+
+    onSpinEnd(symbols) {
+        this.spinButton.disabled = false;
+
+        this.config.onSpinEnd?.(symbols);
+
+        if (this.autoPlayCheckbox.checked) {
+            return window.setTimeout(() => this.spin(), 200);
+        }
+    }
+}
+
+const config = {
+    inverted: false, // true: reels spin from top to bottom; false: reels spin from bottom to top
+    onSpinStart: (symbols) => {
+        console.log("onSpinStart", symbols);
+    },
+    onSpinEnd: (symbols) => {
+        console.log("onSpinEnd", symbols);
+    },
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+    const slot = new Slot(document.getElementById("slot"), config);
+});
+
--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -10,25 +10,6 @@ class GameController extends AbstractController
 {
     #[Route('/', name: 'game', methods: ['GET'])]
     public function viewGame() {
-
-        $images = [
-            "7.png",
-            "fraise.png",
-            "gold.png",
-            "max.png",
-            "pasteque.png",
-            "prune.png",
-            "citron.png",
-            "cerise.png"
-        ];
-
-        $gridImages = [];
-        for ($i = 0; $i < 25; $i++) {
-            $gridImages[] = $images[array_rand($images)];
-        }
-
-        return $this->render('game/game.html.twig', [
-            'gridImages' => $gridImages
-        ]);
+        return $this->render('game/game.html.twig');
     }
 }
