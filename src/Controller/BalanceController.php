e2ca9c9cb5a5e4143120c41b446100f5830267d2
HugoooR <hugo.rochelle@gmail.com>
2024-06-10 23:01:13 +0200

    :construction: add persistance method, need to test

--- src/Controller/BalanceController.php
+++ src/Controller/BalanceController.php
@@ -4,33 +4,52 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Form\ReloadBalanceType;
 
 #[Route('/balance')]
 class BalanceController extends AbstractController
 {
-    #[Route('/recharge')]
-    public function recharge()
+
+    public function __construct(
+        private readonly Security $security,
+    )
     {
+    }
+
+
+
+    #[Route('/recharge')]
+    public function recharge(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $error = null;
+
+        $user = $this->security->getUser();
+    
         $form = $this->createForm(ReloadBalanceType::class);
+        $form->handleRequest($request);
+    
+        if ($form->isSubmitted() && $form->isValid()) {
+            $amount = $form->get('amount')->getData();
+    
+        
+            $currency = $request->request->get('currency');
 
-        // $form->handleRequest($request);
-        // if ($form->isSubmitted() && $form->isValid()) {
-        //     $amount = $form->getData()['amount'];
+            $convertedAmount = $this->convertCurrency($amount, $currency);
+    
+            $user->setBalance($user->getBalance() + $convertedAmount);
+    
+            $entityManager->persist($user);
+            $entityManager->flush();
 
-        //     $user = $this->getUser();
-        //     $user->setBalance($user->getBalance() + $amount);
-
-        //     //$entityManager = $this->getDoctrine()->getManager();
-        //     //$entityManager->persist($user);
-        //     //$entityManager->flush();
-
-        //     $this->addFlash('success', 'Votre compte a été rechargé avec succès.');
-        //     return $this->redirectToRoute('account_recharge');
-        // }
-
+            $error = 'Votre depot a été effectué avec succès.';
+        
+    
+        }
+    
         return $this->render('balance/add-balance.html.twig', [
+            'error' => $error,
             'form' => $form->createView(),
         ]);
     }
