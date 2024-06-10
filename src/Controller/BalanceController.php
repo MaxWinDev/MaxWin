038a6e75ff58199d11939edd3fa81dc60472b79e
Audric Sabatier <Audric.SABATIER@etu.uca.fr>
2024-06-10 22:30:02 +0200

    :sparkle: add: withdraw methods

--- src/Controller/BalanceController.php
+++ src/Controller/BalanceController.php
@@ -6,10 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Form\ReloadBalanceType;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 #[Route('/balance')]
 class BalanceController extends AbstractController
 {
+
+    public function __construct(
+        private readonly Security $security,
+    )
+    {
+    }
+
     #[Route('/recharge')]
     public function recharge()
     {
@@ -33,5 +43,46 @@ class BalanceController extends AbstractController
         return $this->render('balance/add-balance.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+        
+    #[Route('/withdraw', name: 'app_withdraw')]
+    public function withdraw(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->security->getUser();
+
+        $form = $this->createForm(ReloadBalanceType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $amount = $form->get('amount')->getData();
+
+            $currency = $request->request->get('currency');
+
+            $convertedAmount = $this->convertCurrency($amount, $currency);
+
+            $user->setBalance($user->getBalance() - $convertedAmount);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+        }
+
+        return $this->render('balance/withdraw-balance.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    private function convertCurrency($amount, $currency)
+    {
+        switch ($currency) {
+            case 'USD':
+                return $amount * 1.1;
+            case 'GBP':
+                return $amount * 0.9;
+            case 'EUR':
+            default:
+                return $amount;
+        }
     }
 }
