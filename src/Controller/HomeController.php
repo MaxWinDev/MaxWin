0ce4f8d21ee1c11a0795348692d1938918b9f537
thdupin2 <theo.dupin03300@gmail.com>
2024-06-11 11:45:28 +0200

    :hammer: update: get user balance in navbar

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -11,10 +12,18 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[Route("/home")]
 class HomeController extends AbstractController
 {
+    public function __construct(
+        private readonly Security $security,
+    )
+    {
+    }
+
     #[Route("/", name: 'app_home')]
     public function index(): Response
     {
+        $user = $this->security->getUser();
         return $this->render('index.html.twig', [
+            'user' => $user,
             'controller_name' => 'HomeController',
         ]);
     }
