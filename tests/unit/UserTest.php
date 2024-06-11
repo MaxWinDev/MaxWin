09afed7dc108d81eb0c7e5fa9dc4ae3b9e02c547
Destroyeur6315 <romain.fillot630@gmail.com>
2024-06-11 12:00:48 +0200

    new tests unit for user

---
+++ tests/unit/UserTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Service\UserRegistrationService;
+use App\Entity\User;
+use App\Entity\Win;
+
+class UserTest extends TestCase
+{
+    public function test_create_user(){
+        $user = new User();
+
+        $user->setBalance(50);
+        $this->assertEquals(50, $user->getBalance());
+
+        $user->setUsername("destroyeur");
+        $this->assertEquals("destroyeur", $user->getUsername());
+    }
+
+    public function test_add_wins(){
+        $win = new Win();
+        $win2 = new Win();
+        $user = new User();
+
+        $user->addWin($win);
+        $this->assertEquals(1, count($user->getWins()));
+
+        $user->addWin($win2);
+        $this->assertEquals(2, count($user->getWins()));
+    }
+}
+
+?>
